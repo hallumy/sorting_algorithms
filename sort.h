199782a651a7990c1c24d6eb6df73c8c395b505f
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-11-09 05:21:03 +0300

    Header file

--- sort.h
+++ sort.h
@@ -39,6 +39,7 @@ void merge_recursion(int *arr, int *array, size_t left, size_t right);
 void merge_subarray(int *arr, int *array, size_t left,
 		size_t middle, size_t right);
 void heap_sort(int *array, size_t size);
+void sift(int *array, int heap, int i, int size);
 void heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
 void count_sort_LSD(int *array, size_t size, size_t lsd);
